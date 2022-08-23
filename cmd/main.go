69d76b78c29788bd8b4c021ee663049041a659d5
mikha <ya.mikushoff@gmail.com>
2022-08-23 11:33:30 +0300

    refactor

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,10 @@
 package main
 
 import (
-	"bufio"
 	"flag"
 	"fmt"
-	"github.com/clarkduvall/hyperloglog"
-	"github.com/col3name/ip-unique-addr/pkg/hash"
-	"os"
+	"github.com/col3name/ip-unique-addr/pkg/service"
 	"runtime"
-	"sync"
 	"time"
 )
 
@@ -18,78 +14,13 @@ func main() {
 	flag.Parse()
 
 	fmt.Println(*fileName)
-	file, err := os.Open(*fileName)
-	if err != nil {
-		fmt.Println("cannot able to read the file1", err)
-		return
-	}
-
-	stat, err := file.Stat()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(stat.Size())
-
-	partSize := stat.Size() / int64(*countParallelTask)
-	defer func() {
-		_ = file.Close()
-	}()
-	h, _ := hyperloglog.New(16)
 	start := time.Now()
-	files := make([]*os.File, *countParallelTask)
-	wg := sync.WaitGroup{}
-	mu := sync.Mutex{}
-	for i := range files {
-		file, err := os.Open(*fileName)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		if i > 0 {
-			_, err := file.Seek(partSize*int64(i), os.SEEK_CUR)
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-		}
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			hyperLogLog := readWriteFile(file, partSize)
-			mu.Lock()
-			err = h.Merge(hyperLogLog)
-			mu.Unlock()
-			if err != nil {
-				fmt.Println(err)
-				panic(err)
-			}
-		}()
-		files[i] = file
+	counterHLL := service.NewUniqueCounterHLL(*fileName, *countParallelTask)
+	count, err := counterHLL.Count()
+	elapsed := time.Since(start).String()
+	if err != nil {
+		fmt.Println(elapsed, err)
+		return
 	}
-	wg.Wait()
-	for _, file := range files {
-		file.Close()
-	}
-	fmt.Println(time.Since(start).String(), h.Count())
-}
-
-func readWriteFile(input *os.File, count int64) *hyperloglog.HyperLogLog {
-	r := bufio.NewReader(input)
-	h, _ := hyperloglog.New(16)
-	j := int64(0)
-	for j < count-1 {
-		bytes, err := r.ReadBytes('\n')
-		if err != nil {
-			fmt.Println(err)
-			panic(err)
-		}
-		if len(bytes)/1024/1024 > 1 {
-			fmt.Println(len(bytes))
-		}
-		j += int64(len(bytes))
-		h.Add(hash.Hash32(string(bytes)))
-	}
-	fmt.Println(count, j)
-	return h
+	fmt.Println("Time taken", elapsed, "\nUnique", count)
 }
---
+++ pkg/service/counter.go
@@ -0,0 +1,119 @@
+package service
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"github.com/clarkduvall/hyperloglog"
+	"github.com/col3name/ip-unique-addr/pkg/hash"
+	"os"
+	"sync"
+)
+
+type CounterService interface {
+	Count() (uint64, error)
+}
+
+type uniqueCounterHyperLogLog struct {
+	filePath          string
+	countParallelTask int
+	hyperloglog       hyperloglog.HyperLogLog
+}
+
+func NewUniqueCounterHLL(filepath string, countParallelTask int) *uniqueCounterHyperLogLog {
+	return &uniqueCounterHyperLogLog{filePath: filepath, countParallelTask: countParallelTask}
+}
+
+func (c *uniqueCounterHyperLogLog) CountInFile(index int, partSize int64) (*os.File, *hyperloglog.HyperLogLog, error) {
+	if index < 0 {
+		return nil, nil, errors.New("invalid index")
+	}
+	file, err := os.Open(c.filePath)
+	if err != nil {
+		return nil, nil, err
+	}
+	_, err = file.Seek(partSize*int64(index), os.SEEK_CUR)
+	if err != nil {
+		return nil, nil, err
+	}
+	hll, err := c.readFile(file, partSize)
+
+	return file, hll, err
+}
+
+func (c *uniqueCounterHyperLogLog) Count() (uint64, error) {
+	partSize, err := c.getPartSize()
+	if err != nil {
+		return 0, err
+	}
+	h, _ := hyperloglog.New(16)
+	files := make([]*os.File, c.countParallelTask)
+	wg := sync.WaitGroup{}
+	mu := sync.Mutex{}
+	for i := range files {
+		c.handleFileAsync(&wg, partSize, &mu, h, files, i)
+	}
+	wg.Wait()
+	c.closeAllFiles(files)
+
+	count := h.Count()
+	h.Clear()
+	return count, nil
+}
+
+func (c *uniqueCounterHyperLogLog) closeAllFiles(files []*os.File) {
+	for _, file := range files {
+		file.Close()
+	}
+}
+
+func (c *uniqueCounterHyperLogLog) getPartSize() (int64, error) {
+	file, err := os.Open(c.filePath)
+	if err != nil {
+		return 0, err
+	}
+	stat, err := file.Stat()
+	if err != nil {
+		return 0, err
+	}
+	partSize := stat.Size() / int64(c.countParallelTask)
+	defer func() {
+		_ = file.Close()
+	}()
+	return partSize, nil
+}
+
+func (c *uniqueCounterHyperLogLog) handleFileAsync(wg *sync.WaitGroup, partSize int64, mu *sync.Mutex, h *hyperloglog.HyperLogLog, files []*os.File, i int) {
+	wg.Add(1)
+	go func(i int) {
+		defer wg.Done()
+		file, hll, err := c.CountInFile(i, partSize)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		mu.Lock()
+		defer mu.Unlock()
+		err = h.Merge(hll)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		files[i] = file
+	}(i)
+}
+
+func (c *uniqueCounterHyperLogLog) readFile(input *os.File, count int64) (*hyperloglog.HyperLogLog, error) {
+	r := bufio.NewReader(input)
+	h, _ := hyperloglog.New(16)
+	j := int64(0)
+	for j < count-1 {
+		bytes, err := r.ReadBytes('\n')
+		if err != nil {
+			return nil, err
+		}
+		j += int64(len(bytes))
+		h.Add(hash.Hash32(string(bytes)))
+	}
+	return h, nil
+}
